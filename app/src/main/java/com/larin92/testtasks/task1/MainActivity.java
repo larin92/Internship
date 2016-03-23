d9318f78f2cdfa88984c05885f2f4b06351b1f06
larin92 <larkin9292@gmail.com>
2016-03-23 14:29:06 +0200

    Recycler Adaption, screen supports

--- app/src/main/java/com/larin92/testtasks/task1/MainActivity.java
+++ app/src/main/java/com/larin92/testtasks/task1/MainActivity.java
@@ -2,6 +2,8 @@ package com.larin92.testtasks.task1;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
@@ -11,13 +13,20 @@ import android.widget.Toast;
 import com.yalantis.starwars.TilesFrameLayout;
 import com.yalantis.starwars.interfaces.TilesFrameLayoutListener;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 import butterknife.Bind;
 import butterknife.ButterKnife;
+
 
 public class MainActivity extends AppCompatActivity {
 
     @Bind(R.id.tiles_frame_layout) TilesFrameLayout mTilesFrameLayout;
     @Bind(R.id.toolbar) Toolbar mToolbar;
+    @Bind(R.id.rv) RecyclerView mRecycler;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,12 +34,29 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         ButterKnife.bind(this);
 
+        //sets up the Back Button
         setSupportActionBar(mToolbar);
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
 
+        //populate our recyclerView
+        populateRecycler();
+
+        //funky stuff
         starWars();
     }
 
+    //populate our recyclerView
+    protected void populateRecycler(){
+        List<String> mUrls = new ArrayList<>(Arrays.
+                asList(getResources().
+                        getStringArray(R.array.urls)));
+
+        mRecycler.setLayoutManager(new LinearLayoutManager(this, RecyclerView.HORIZONTAL, false));
+        MyRecyclerAdapter adapter = new MyRecyclerAdapter(this, mUrls);
+        mRecycler.setAdapter(adapter);
+    }
+
+    //funky stuff
     private void starWars()
     {
         TilesFrameLayoutListener listener = new TilesFrameLayoutListener() {
@@ -48,6 +74,10 @@ public class MainActivity extends AppCompatActivity {
         Toast.makeText(this, name, Toast.LENGTH_SHORT).show();
     }
 
+    public void onClickToastR(View view) {
+        Toast.makeText(this, "RecyclerView", Toast.LENGTH_SHORT).show();
+    }
+
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
@@ -55,6 +85,8 @@ public class MainActivity extends AppCompatActivity {
         return true;
     }
 
+    //flag to avoid double-activating Star Wars animation
+    boolean flag=true;
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
         // Handle action bar item clicks here. The action bar will
@@ -62,13 +94,13 @@ public class MainActivity extends AppCompatActivity {
         // as you specify a parent activity in AndroidManifest.xml.
         int id = item.getItemId();
 
-        //noinspection SimplifiableIfStatement
         if (id == R.id.action_settings) {
             Toast.makeText(this, "action_settings", Toast.LENGTH_SHORT).show();
             return true;
         }
 
-        if (id == android.R.id.home) {
+        if ((id == android.R.id.home) && flag) {
+            flag=false;
             mTilesFrameLayout.startAnimation();
         }
 
---
+++ app/src/main/java/com/larin92/testtasks/task1/MyRecyclerAdapter.java
@@ -0,0 +1,60 @@
+package com.larin92.testtasks.task1;
+
+import android.content.Context;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+
+import com.bumptech.glide.Glide;
+
+import java.util.List;
+
+import butterknife.Bind;
+import butterknife.ButterKnife;
+
+
+public class MyRecyclerAdapter extends RecyclerView.Adapter<MyRecyclerAdapter.mHolder> {
+    private Context mContext;
+    protected List<String> mImages;
+
+    public MyRecyclerAdapter(Context context, List<String> images) {
+        mContext = context;
+        mImages = images;
+    }
+
+    // get the array size
+    @Override
+    public int getItemCount() {
+        return mImages.size();
+    }
+
+    protected class mHolder extends RecyclerView.ViewHolder {
+        @Bind(R.id.image) ImageView mImageView;
+
+        public mHolder(View v) {
+            super(v);
+            ButterKnife.bind(this, v);
+        }
+    }
+
+    // create new view
+    @Override
+    public MyRecyclerAdapter.mHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        View v = LayoutInflater.from(mContext).inflate(R.layout.recycler_item, parent, false);
+        return new mHolder(v);
+    }
+
+    // set the image in the newborn view
+    @Override
+    public void onBindViewHolder(mHolder holder, int position) {
+        // get the image from URL
+        Glide
+                .with(mContext)
+                .load(mImages.get(position))
+                .placeholder(R.drawable.placeholder)
+                .crossFade()
+                .into(holder.mImageView);
+    }
+}
