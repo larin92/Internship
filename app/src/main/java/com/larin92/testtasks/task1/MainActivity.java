d7e88dec47184f265c390f0524fc4d8197355d75
larin92 <larkin9292@gmail.com>
2016-03-31 02:13:05 +0300

    just some funk

--- app/src/main/java/com/larin92/testtasks/task1/MainActivity.java
+++ app/src/main/java/com/larin92/testtasks/task1/MainActivity.java
@@ -77,6 +77,7 @@ public class MainActivity extends AppCompatActivity {
         TilesFrameLayoutListener listener = new TilesFrameLayoutListener() {
             @Override
             public void onAnimationFinished() {
+                mp.stop();
                 finish();
             }
         };
